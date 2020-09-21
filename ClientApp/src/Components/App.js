fb24cd4dea4cebcbe9d5e49396664404fe7d7d6e
Biplab Thapa Magar <biplabthapamagar@biplabs-mbp.lan>
2020-09-21 02:39:12 -0400

    completed navigation bar

--- ClientApp/src/Components/App.js
+++ ClientApp/src/Components/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from './NavBar';
 import '../CSS/App.css';
+import Home from '../Pages/Home.js';
+import Projects from '../Pages/Projects.js';
+import Reports from '../Pages/Reports.js';
+import Tasks from '../Pages/Tasks.js';
 
 const App = () => {
     return(
         <div>
             <BrowserRouter>
-                <NavBar/>
-                <Switch>
-                    <Route path="/" />
-                </Switch>
+                <div>
+                    {/* <Header /> */}
+                    <NavBar/>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/projects" exact component={Projects}/>
+                    <Route path="/reports" exact component={Reports}/>
+                    <Route path="/tasks" exact component={Tasks}/>
+                </div>
             </BrowserRouter>
         </div> 
     );
--- ClientApp/src/Components/NavBar.js
+++ ClientApp/src/Components/NavBar.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AiOutlineBars } from 'react-icons/ai';
 import { NavBarItems } from './NavBarItems.js';
 import '../CSS/NavBar.css';
 
 const NavBar = () => {
+    const [currentLocation, setCurrentLocation] = useState(window.location.pathname);
+
+    const onLocationChange = () =>{
+        setCurrentLocation(window.location.pathname);
+    }
+
     return (
         <div className="navigation">
             <nav className="nav-bar">
                 <ul className="nav-menu-items">
                     {NavBarItems.map((item, index) => {
                         return (
-                            <li key={index} className={item.className}>
-                                <Link to={item.path}>
+                            <li key={index} className={item.className} onClick={onLocationChange}>
+                                <Link to={item.path} className={currentLocation === item.path ? "active" : ""}>
                                     {item.icon}
                                     <span>{item.title}</span>
                                 </Link> 
